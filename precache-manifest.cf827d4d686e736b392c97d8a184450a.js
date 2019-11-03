bc1efcc56b629c25cc45e44c34694a408f519446
AFNAN NAVAZ <afnan@pop-os.localdomain>
2019-11-03 21:52:40 +0530

    Updates

---
+++ precache-manifest.cf827d4d686e736b392c97d8a184450a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b480bf9fab0bc0adc567a6911c2dc2c",
+    "url": "/Brihaspathi/index.html"
+  },
+  {
+    "revision": "f824ca4037285147ecdc",
+    "url": "/Brihaspathi/static/css/2.bfe70ee7.chunk.css"
+  },
+  {
+    "revision": "5df2cfbae02d1f55680a",
+    "url": "/Brihaspathi/static/css/3.94142203.chunk.css"
+  },
+  {
+    "revision": "647e5f629bc7f45e991b",
+    "url": "/Brihaspathi/static/css/4.c78b3d5e.chunk.css"
+  },
+  {
+    "revision": "2e002ec17371f0ad93ed",
+    "url": "/Brihaspathi/static/css/main.1acdfdb8.chunk.css"
+  },
+  {
+    "revision": "f824ca4037285147ecdc",
+    "url": "/Brihaspathi/static/js/2.21ebc1e6.chunk.js"
+  },
+  {
+    "revision": "5df2cfbae02d1f55680a",
+    "url": "/Brihaspathi/static/js/3.38fcfff9.chunk.js"
+  },
+  {
+    "revision": "647e5f629bc7f45e991b",
+    "url": "/Brihaspathi/static/js/4.210f49ec.chunk.js"
+  },
+  {
+    "revision": "1b306e2eaa01024b805a",
+    "url": "/Brihaspathi/static/js/5.ec48908c.chunk.js"
+  },
+  {
+    "revision": "2e002ec17371f0ad93ed",
+    "url": "/Brihaspathi/static/js/main.39020dfb.chunk.js"
+  },
+  {
+    "revision": "18993d97fc2bcac9f6e9",
+    "url": "/Brihaspathi/static/js/runtime-main.c553dc7d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Brihaspathi/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Brihaspathi/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Brihaspathi/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Brihaspathi/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Brihaspathi/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
---
+++ static/js/5.ec48908c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbrihaspathi=this.webpackJsonpbrihaspathi||[]).push([[5],{50:function(t,n,e){"use strict";e.r(n);var a=e(0),c=e.n(a),s=e(11);n.default=function(t){var n=t.history,e=t.match;return Object(a.useEffect)((function(){Object(s.c)(e.params.event).then((function(t){console.log(t),n.push("/prelims/".concat(e.params.event,"/").concat(t.questions[0]))}))})),c.a.createElement("div",null,"Loading")}}}]);
+//# sourceMappingURL=5.ec48908c.chunk.js.map
